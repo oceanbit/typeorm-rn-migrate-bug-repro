d5e04ececbab2db6493025ec2c1539e4e8201170
Corbin Crutchley <crutchcorn@gmail.com>
2021-07-02 03:58:37 -0700

    Fix behavior using "babel-node" alternative

---
+++ babel-register.js
@@ -0,0 +1,5 @@
+require("@babel/register")({
+  ignore: []
+});
+
+require("./node_modules/typeorm/cli.js");
--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,3 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['@babel/plugin-proposal-decorators', {legacy: true}],
-    [
-      'module-resolver',
-      {
-        cwd: 'babelrc',
-        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
-        alias: {
-          '@components': './src/components',
-          '@constants': './src/constants',
-          '@entities': './src/entities',
-          '@hooks': './src/hooks',
-          '@types': './src/types',
-          '@utils': './src/utils',
-          '@store': './src/store',
-          '@assets': './assets',
-          '@services': './src/services',
-        },
-      },
-    ],
-  ],
+  presets: ['module:metro-react-native-babel-preset']
 };
